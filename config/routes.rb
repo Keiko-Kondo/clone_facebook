d2786288b7ab78aeff9f3fe9243b6d65fc848234
keiko kondo <keiko.ayaka@icloud.com>
2020-08-13 16:06:12 +0900

    pictureno

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  resources :pictures
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
   resources :blogs do
+    collection do
+      post :confirm
+    end
+  end
+  resources :pictures do
     collection do
       post :confirm
     end

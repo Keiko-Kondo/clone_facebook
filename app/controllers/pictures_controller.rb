4ff6e5e12688b6c6d77801b19f0b60e0fddb16ae
keiko kondo <keiko.ayaka@icloud.com>
2020-08-20 16:32:05 +0900

    コメントのcreateができるように修正

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -3,8 +3,10 @@ class PicturesController < ApplicationController
   def index
     @pictures = Picture.all
   end
+
   def show
   end
+
   def new
     if params[:back]
       @picture = Picture.new(picture_params)
@@ -12,20 +14,28 @@ class PicturesController < ApplicationController
       @picture = Picture.new
     end
   end
+
   def edit
   end
+
   def create
     @picture = current_user.pictures.build(picture_params)
-    if @picture.save
-      redirect_to @picture, notice: 'Picture was successfully created.'
-    else
+    if params[:back]
       render :new
+    else
+      if @picture.save
+        redirect_to new_picture_path, notice: 'Picture was successfully created.'
+      else
+        render :new
+      end
     end
   end
+
   def confirm
     @picture = current_user.pictures.build(picture_params)
     render :new if @picture.invalid?
   end
+
   def update
     if @picture.update(picture_params)
       redirect_to pictures_path, notice: 'Picture was successfully updated.'
@@ -33,15 +43,18 @@ class PicturesController < ApplicationController
       render :edit
     end
   end
+
   def destroy
     @picture.destroy
     redirect_to pictures_url, notice: 'Picture was successfully destroyed.'
   end
+
   private
   def set_picture
     @picture = Picture.find(params[:id])
   end
+  
   def picture_params
-    params.require(:picture).permit(:image, :image_cache)
+    params.require(:picture).permit(:image, :comment, :image_cache)
   end
 end
